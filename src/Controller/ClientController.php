b3f3d5835562edd78ec7f7738bf5af8b9109ae25
lionel-kg <lionel77350@gmail.com>
2022-05-15 23:46:11 +0200

    add error

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -44,6 +44,9 @@ class ClientController extends CustomAbstractController
             "errorDebug" => $errorDebug,
             "user" => $user,
         ] = $userService->add($newParameters,ClientEntity::class);
+        if($errorDebug !== ""){
+            $this->sendError($error,$errorDebug);
+        }
         return $this->sendSuccess("User created success",$user, response::HTTP_CREATED);
     }
 
